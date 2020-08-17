1133497f267cd5d07052342ef4a1aaec0fe4493e
Anthony Tulumello <anthonytulumello@gmail.com>
2020-08-17 11:13:37 -0400

    Update firebase.js

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase'
 
 const config = {
-    apiKey: "AIzaSyAsNwU0TQq72Sa3sSodK6IjJCMLPM-Nk_U",
-    authDomain: "greater-toronto-financial.firebaseapp.com",
-    databaseURL: "https://greater-toronto-financial.firebaseio.com",
-    projectId: "greater-toronto-financial",
-    storageBucket: "greater-toronto-financial.appspot.com",
-    messagingSenderId: "707281007438",
-    appId: "1:707281007438:web:9ec4b191f2f753aeef241f",
-    measurementId: "G-R03NQP6SS8"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 
 
