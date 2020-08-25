09542e33a7e299bbd57515dc6cae1c8306766e5b
Anthony Tulumello <anthonytulumello@gmail.com>
2020-08-25 16:12:19 -0400

    Added env variables

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,15 +1,15 @@
 import firebase from 'firebase'
 
 const config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
-  }
+  apiKey: `${process.env.FIREBASE_APIKEY}`,
+  authDomain: `${process.env.FIREBASE_AUTHDOMAIN}`,
+  databaseURL: `${process.env.FIREBASE_DATABASEURL}`,
+  projectId: `${process.env.FIREBASE_PROJECTID}`,
+  storageBucket: `${process.env.FIREBASE_STORAGEBUCKET}`,
+  messagingSenderId: `${process.env.FIREBASE_SENDERID}`,
+  appId: `${process.env.FIREBASE_APPID}`,
+  measurementId: `${process.env.FIREBASE_MEASUREMENTID}`
+}
 
 
 firebase.initializeApp(config)
